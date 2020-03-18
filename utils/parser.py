6642adcc56f2f3d8fc06771910b9047fc545f9dc
Erik Olsvik Dengerud <eriko1306@gmail.com>
2020-03-18 16:16:23 +0100

    Fixed booleans in parser.

--- utils/parser.py
+++ utils/parser.py
@@ -1,5 +1,11 @@
 import argparse
 
+def add_bool_arg(parser, name, default=False):
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--' + name, dest=name, action='store_true')
+    group.add_argument('--no-' + name, dest=name, action='store_false')
+    parser.set_defaults(**{name:default})
+
 def parse():
     parser = argparse.ArgumentParser(description='Adding Problem')
     parser.add_argument(
@@ -21,13 +27,9 @@ def parse():
     parser.add_argument(
         '--res_block_size', type=int, default=32, metavar='res_block_size')
     parser.add_argument(
-        '--bias', type=bool, default=True, metavar='bias')
-    parser.add_argument(
         '--dropout', type=float, default=0.0, metavar='dropout')
     parser.add_argument(
         '--stride', type=int, default=1, metavar='stride')
-    parser.add_argument(
-        '--leveledinit', type=bool, default=False, metavar='leveledinit')
     parser.add_argument(
         '--model_save_path', type=str, default='electricity/models/tcn_electricity.pt', 
         metavar='model_save_path')
@@ -36,25 +38,24 @@ def parse():
     parser.add_argument(
         '--lr', type=float, default=5e-4, metavar='lr')
     parser.add_argument(
-        '--clip', type=bool, default=False, metavar='clip')
-    parser.add_argument(
         '--log_interval', type=int, default=5, metavar='log_interval')
     parser.add_argument(
         '--writer_path', type=str, default='electricity/runs/', 
         metavar='writer_path')
-    parser.add_argument(
-        '--writer_comment', type=str, default='test', 
-        metavar='writer_comment')
-    parser.add_argument(
-        '--print', type=bool, default=False, metavar='print')
     parser.add_argument(
         '--num_workers', type=int, default=0, metavar='num_workers')
     parser.add_argument(
         '--num_rolling_periods', type=int, default=7, metavar='num_rolling')
     parser.add_argument(
         '--length_rolling', type=int, default=24, metavar='length_rolling')
-    parser.add_argument(
-        '--time_covariates', type=bool, default=False)
+
+    # Booleans
+    # Becomes e.g. --bias and --no-bias
+    add_bool_arg(parser, name='bias', default=True)
+    add_bool_arg(parser, name='leveledinit', default=True)
+    add_bool_arg(parser, name='clip', default=True)
+    add_bool_arg(parser, name='time_covariates', default=True)
+    add_bool_arg(parser, name='print', default=False)
 
     args = parser.parse_args()
     return args

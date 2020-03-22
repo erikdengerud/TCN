d5e3826e8a930369f0a976c7de20e8aeaf6e3bf3
Erik Olsvik Dengerud <eriko1306@gmail.com>
2020-03-22 18:21:35 +0100

    preds init

---
+++ electricity/plot_predictions.py
@@ -0,0 +1,24 @@
+# plot_predictions.py
+"""
+Plotting of the predictions from the models in models/.
+Plotting the predictions for the test window 7x24h.
+"""
+
+def plot_predictions(model, dataset, save_path):
+    # load model
+
+    # load data
+
+    # predict using multi step and rolling predictions
+
+    # plot n series at a time. Real and predicted values
+
+    # save plots
+
+
+if __name__ == "__main__":
+    # Create test dataset
+
+    # for model in models
+    
+    # plot predictions

0cb63599808b31cf1676b8258ad190dc9dd722fb
Erik Olsvik Dengerud <eriko1306@gmail.com>
2020-03-26 20:02:18 +0100

    Cleaned adding problem.

--- adding_problem/model.py
+++ adding_problem/model.py
@@ -1,10 +1,10 @@
 # model.py
-
 import torch.nn as nn
+import torch.tensor as Tensor
 
 import sys
 
-sys.path.append('')
+sys.path.append("")
 sys.path.append("../../")
 
 from TCN.tcn import TemporalConvolutionalNetwork
@@ -13,16 +13,17 @@ from TCN.tcn import TemporalConvolutionalNetwork
 class TCN(nn.Module):
     def __init__(
         self,
-        num_layers,
-        in_channels,
-        out_channels,
-        residual_blocks_channel_size,
-        dilations=None,
-        kernel_size=3,
-        bias=True,
-        dropout=0.5,
-        stride=1,
-        leveledinit=False):
+        num_layers: int,
+        in_channels: int,
+        out_channels: int,
+        residual_blocks_channel_size: List[int],
+        dilations: List[int] = None,
+        kernel_size: int = 3,
+        bias: bool = True,
+        dropout: float = 0.5,
+        stride: int = 1,
+        leveledinit: bool = False,
+    ) -> None:
         super(TCN, self).__init__()
         self.tcn = TemporalConvolutionalNetwork(
             num_layers,
@@ -34,16 +35,15 @@ class TCN(nn.Module):
             bias,
             dropout,
             stride,
-            leveledinit
+            leveledinit,
         )
         self.linear = nn.Linear(residual_blocks_channel_size[-1], out_channels)
         self.init_weights()
-    
-    def init_weights(self):
-        self.linear.weight.data.normal_(0, 0.01)
-    
-    def forward(self, x):
-        out = self.tcn(x)
-        out = self.linear(out[:,:,-1])
-        return out
 
+    def init_weights(self) -> None:
+        self.linear.weight.data.normal_(0, 0.01)
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.tcn(x)
+        out = self.linear(out[:, :, -1])
+        return out

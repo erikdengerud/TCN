511988fa0d10ef07142d1b649b8950037a1086a9
Erik Olsvik Dengerud <eriko1306@gmail.com>
2020-06-10 19:27:52 +0200

    before branch

--- utils/cs_clustering_example.py
+++ utils/cs_clustering_example.py
@@ -5,7 +5,7 @@ from matplotlib.colors import ListedColormap
 
 rc("text", usetex=True)
 
-from sklearn.cluster import KMeans, SpectralClustering
+from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
 from sklearn.datasets import make_moons
 from sklearn.preprocessing import StandardScaler
 
@@ -21,11 +21,14 @@ spectral = SpectralClustering(
     n_clusters=2, eigen_solver="arpack", affinity="nearest_neighbors"
 ).fit(X)
 kmeans = KMeans(n_clusters=2).fit(X)
+aggl = AgglomerativeClustering(n_clusts=2).fit(X)
+
+
 print("Done clustering")
 print("Plotting...")
 colors = ["#FFC000", "#00B0F0"]
 
-size = 4.77 / 2
+size = 4.77 / 3
 plt.figure(figsize=(size, size))
 plt.scatter(
     X[:, 0],
@@ -53,3 +56,17 @@ plt.scatter(
 # plt.show()
 plt.axis("off")
 plt.savefig("kmeans.pdf", bbox_inches="tight")
+
+plt.figure(figsize=(size, size))
+plt.scatter(
+    X[:, 0],
+    X[:, 1],
+    s=10,
+    c=aggl.labels_,
+    cmap=ListedColormap(colors),
+    edgecolors="black",
+    linewidths=0.1,
+)
+# plt.show()
+plt.axis("off")
+plt.savefig("agglomerative.pdf", bbox_inches="tight")
